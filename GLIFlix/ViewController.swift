9f0a0016fec28553e8b1526e0f4b912cb80185e8
LowSpecx <mauricetin.916@gmail.com>
2023-01-12 15:02:18 +0700

    Initial Commit

---
+++ GLIFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GLIFlix
+//
+//  Created by Maurice Tin on 12/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
